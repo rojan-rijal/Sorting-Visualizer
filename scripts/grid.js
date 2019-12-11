6f8142a68b108eac89ff667f9e1da3b66fec89c4
Jacob Powell <jcbpowell12@gmail.com>
2019-12-11 03:48:34 -0800

    tester class with the drawing of three graphs

--- scripts/grid.js
+++ scripts/grid.js
@@ -4,16 +4,17 @@ let scale_factor = BOARD_WIDTH;
 let width = Math.pow(BOARD_WIDTH,2);
 let height = Math.pow(BOARD_HEIGHT,2);
 
-let merge = document.getElementById("merge");
-let quick = document.getElementById("quick");
-let insert = document.getElementById("insert");
+let merge = document.getElementById('merge');
+let quick = document.getElementById('quick');
+let insert = document.getElementById('insert');
 
 let merge_context = contextGenerator(merge)
 let quick_context = contextGenerator(quick)
 let insert_context = contextGenerator(insert)
 
-console.log(merge);
-console.log(merge_context);
+let merge_button = document.getElementById('merge_button');
+let quick_button = document.getElementById('quick_button');
+let insert_button = document.getElementById('insert_button');
 
 setDimensions(merge);
 setDimensions(quick);
@@ -33,10 +34,9 @@ const MAX_SUM = 16;
 
 const random_mode = false;
 
-let grid = [];
-let matrix = [];
-
-let visited = [];
+let merge_grid = [];
+let quick_grid = [];
+let insert_grid = [];
 
 //OBJECTS
 
@@ -70,14 +70,14 @@ class Quicksort{
      let pivot = inputArr[low];
 
      //Left "Dog", looking for something smaller than pivot
-     var i = low;
+     let i = low;
      while(inputArr[i] < pivot)
      {
        i++
      }
 
      //Right "Dog", looking for something bigger than pivot
-     var j = inputArr[high];
+     let j = inputArr[high];
      while(inputArr[j] > pivot)
      {
        j--;
@@ -89,7 +89,7 @@ class Quicksort{
 
 //Used to swap two numbers in an array
   swap(index1, index2){
-    var temp = myArr[index1];
+    let temp = myArr[index1];
     inputArr[index1] = myArr[index2];
     inputArr[index2] = temp;
   }
@@ -116,12 +116,11 @@ class Coord{
 }
 
 class Node{
-  constructor(position){
-    this.position = position;
-    this.random_pos = null;
-    this.width = BOARD_WIDTH;
-    this.height = BOARD_HEIGHT;
-    this.adj_list = [];
+  constructor(position, value){
+      this.position =  position,
+      this.width = BOARD_WIDTH,
+      this.height = BOARD_HEIGHT,
+      this.value = value
   }
 }
 
@@ -139,9 +138,7 @@ function hasBeenVisited(context, node){
 
 //DRAWING FUNCTIONS
 
-function drawBoard(context, index){
-  console.log(":D");
-  index = index + 1;
+function drawBoard(context, index, grid){
   context.beginPath();
   context.strokeStyle = "gray";
   context.fillStyle=  "black";
@@ -149,7 +146,7 @@ function drawBoard(context, index){
   for(var i = 0; i < 12; i++){
     grid[i] = [];
     for(var j = 0; j < 20; j++){
-      grid[i][j] = new Node(i, j, index)
+      grid[i][j] = new Node(new Coord(i, j, 0),  i)
       context.rect(i*BOARD_HEIGHT, j*BOARD_WIDTH, BOARD_WIDTH, BOARD_HEIGHT);
       context.fillRect(i*BOARD_HEIGHT, j*BOARD_WIDTH, BOARD_WIDTH, BOARD_HEIGHT);
     }
@@ -157,39 +154,108 @@ function drawBoard(context, index){
   context.stroke();
 }
 
-function drawNode(context, node, isBotNode){
+function drawNode(context, node){
   context.beginPath();
   context.strokeStyle = "gray";
-  if(isBotNode){
-    node.random_pos = node.position;
-    context.fillStyle = "red";
-  } else {
-    context.fillStyle=  "yellow";
-  }
-  if(random_mode){
-    context.rect(node.random_pos.x*index*scale_factor, node.random_pos.y*index*scale_factor, node.width, node.height);
-    context.fillRect(node.random_pos.x*index*scale_factor, node.random_pos.y*index*scale_factor, node.width, node.height);
-  } else {
-    context.rect(node.position.x*scale_factor, node.position.y*scale_factor, node.width, node.height);
-    context.fillRect(node.position.x*scale_factor, node.position.y*scale_factor, node.width, node.height);
-  }
+  context.fillStyle=  "yellow";
+  context.rect(node.position.x*scale_factor, node.position.y*scale_factor, node.width, node.height);
+  context.fillRect(node.position.x*scale_factor, node.position.y*scale_factor, node.width, node.height);
   context.stroke();
 }
 
-//UNIT TESTING
+//TESTING
 class Tester {
-}
 
+  constructor(grid, context){
+    this.unsorted_array = [];
+    this.grid = grid;
+    this.context = context;
+
+    for(var i = 0; i < 12; i++){
+      this.unsorted_array.push(new Node(new Coord(i, 12-i, 0), i))
+    }
+  }
+
+  //nodes will be Node objects
+  sort(all_nodes, context){
+    for(var i = 0; i < all_nodes.length; i++){
+      if(i != all_nodes.length - 1){
+        // console.log(all_nodes[i].contents.value, all_nodes[i+1].contents.value)
+        if(all_nodes[i].value < all_nodes[i+1].value){
+          // console.log(all_nodes);
+          // console.log("Before: " + all_nodes[i].contents.value)
+          this.swap(all_nodes[i], all_nodes[i+1], context);
+          // console.log("After: " + all_nodes[i].contents.value)
+          //pause momentarily
+        }
+      }
+    }
+  }
+
+  swap(greater, lesser, context){
+    //erase old nodes
+    this.eraseNode(greater, context);
+    this.eraseNode(lesser, context);
+    //swap node objects
+    this.swapNode(greater, lesser);
+    //draw nodes on specific canvas
+    this.drawNode(greater, context);
+    this.drawNode(lesser, context);
+  }
+
+  //helpers
+  eraseNode(node, context, grid){
+    for(var i = 0; i < node.position.x; i++){
+      for(var j = 0; j < node.position.y; j++){
+        context.clearRect(this.grid[i][j].position.x*scale_factor, this.grid[i][j].position.y*scale_factor, this.grid[i][j].width, this.grid[i][j].height);
+      }
+    }
+  }
+
+  swapNode(greater, lesser){
+    for(let property in greater){
+      if(property == "position"){
+        for(var coord in greater[property]){
+          var buffer = greater[property][coord]
+          greater[property][coord] = lesser[property][coord];
+          lesser[property][coord] = buffer;
+        }
+      } else {
+        let buffer = greater[property];
+        greater[property] = lesser[property];
+        lesser[property] = buffer;
+      }
+    }
+  }
+
+  drawNode(node, context){
+    for(var i = 0; i < node.position.x; i++){
+      for(var j = 0; j < node.position.y; j++){
+        context.beginPath();
+        
+        context.strokeStyle = "gray";
+        context.fillStyle = "red";
+        context.rect((+this.grid[i][j].position.x)*scale_factor, this.grid[i][j].position.y*scale_factor, this.grid[i][j].width, this.grid[i][j].height);
+        context.fillRect(this.grid[i][j].position.x*scale_factor, this.grid[i][j].position.y*scale_factor, this.grid[i][j].width, this.grid[i][j].height);
+        context.stroke();
+      }
+    }
+  }
+}
 
 //RUNNING UNIT TESTS
 
-let tester = new Tester();
-
 //MAIN RUNNING
 
-let bot = new Node(new Coord(16, 0, 0));
+drawBoard(merge_context, 0, merge_grid);
+drawBoard(quick_context, 1, quick_grid);
+drawBoard(insert_context, 2, insert_grid);
 
-drawBoard(merge_context, 0);
-drawBoard(quick_context, 1);
-drawBoard(insert_context, 2);
+let tester1 = new Tester(merge_grid, merge_context);
+let tester2 = new Tester(quick_grid, quick_context);
+let tester3 = new Tester(insert_grid, insert_context);
+
+tester1.sort(tester1.unsorted_array, tester1.context);
+tester2.sort(tester2.unsorted_array, tester2.context);
+tester3.sort(tester3.unsorted_array, tester3.context);
 // let interval = setInterval(() => { moveAnt(myBot); }, 20
